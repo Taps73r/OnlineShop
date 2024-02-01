c6ffce07c15044b9cfff3a2c5f53972a8528bbdd
Taps73r <povroznykmisha@gmail.com>
2024-02-01 21:22:15 +0200

    add react hook form

--- src/components/Registration/RegistrationForm/RegistrationForm.tsx
+++ src/components/Registration/RegistrationForm/RegistrationForm.tsx
@@ -1,7 +1,18 @@
 import "./RegistrationForm.scss";
+import { useForm, SubmitHandler } from "react-hook-form";
 
-export function RegistrationForm() {
-    return <div>
-        
-    </div>;
+export function RegistrationForm(): JSX.Element {
+    return (
+        <form>
+            <h1>Registration</h1>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" />
+            </div>
+        </form>
+    );
 }
