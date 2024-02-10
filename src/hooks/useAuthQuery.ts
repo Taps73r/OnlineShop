767618bdb6e986b7c3e9fb95ce1a5f41a56c04bc
Taps73r <povroznykmisha@gmail.com>
2024-02-10 21:38:06 +0200

    Add credentials

--- src/hooks/useAuthQuery.ts
+++ src/hooks/useAuthQuery.ts
@@ -1,11 +1,12 @@
+import { ICredentials } from "@/interface/authorization.interface";
 import { AuthService } from "@/services/auth.service";
 import { useQuery } from "@tanstack/react-query";
 
-export const useAuthQuery = () => {
+export const useAuthQuery = (credentials: ICredentials) => {
     const authService = new AuthService();
 
     return useQuery({
         queryKey: ["user"],
-        queryFn: () => authService.loginUser(),
-    })
+        queryFn: () => authService.loginUser(credentials),
+    });
 };
