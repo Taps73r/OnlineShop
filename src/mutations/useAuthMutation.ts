158a670aac1c63db93907037ee6e6d8c573a8aab
Taps73r <povroznykmisha@gmail.com>
2024-02-11 21:25:51 +0200

    Do useMutation with types

--- src/mutations/useAuthMutation.ts
+++ src/mutations/useAuthMutation.ts
@@ -1,14 +1,22 @@
 import { ICredentials } from "@/interface/authorization.interface";
 import { AuthService } from "@/services/auth.service";
-import { useMutation } from "@tanstack/react-query";
+import { UseMutationResult, useMutation } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
 
-export const authMutation = () => {
+export const useAuthMutation = (): UseMutationResult<
+    AxiosResponse<{ token: string }, any>,
+    Error,
+    ICredentials,
+    unknown
+> => {
     const authService = new AuthService();
-    useMutation(
-        ["login user"],
-        (credentials: ICredentials) => authService.loginUser(credentials),
-        {
-            
-        }
-    );
+
+    return useMutation({
+        mutationFn: (
+            credentials: ICredentials
+        ): Promise<AxiosResponse<{ token: string }>> => {
+            const response = authService.loginUser(credentials);
+            return response;
+        },
+    });
 };
