8ba3b1ca9d2986e61e8364a31382addd81c142fc
Taps73r <povroznykmisha@gmail.com>
2024-02-22 21:40:49 +0200

    Delete mutations folder. Add mutations to hooks folder

--- src/hooks/useAuthMutation.ts
+++ src/hooks/useAuthMutation.ts
@@ -1,5 +1,6 @@
 import { ICredentials } from "@/interface/authorization.interface";
 import { AuthService } from "@/services/auth.service";
+
 import { UseMutationResult, useMutation } from "@tanstack/react-query";
 import { AxiosResponse } from "axios";
 
