d2b18586a62fe3c09e31d287702c680b6c168faa
Taps73r <povroznykmisha@gmail.com>
2024-03-04 23:01:56 +0200

    update error form message

--- src/components/common/ErrorForm.tsx
+++ src/components/common/ErrorForm.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import "./ErrorForm.scss";
+
 interface ErrorFormProps {
     message?: string;
 }
 
 export function ErrorForm({ message }: ErrorFormProps): JSX.Element {
-    return <div className="">{message}</div>;
+    return <div className="error-form">*{message}</div>;
 }
