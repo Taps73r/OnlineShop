0d31c9e1ae10367a333f6bc8e56002be86c297a4
Taps73r <povroznykmisha@gmail.com>
2024-03-06 19:13:06 +0200

    add links with icons

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,8 @@
 import "./Header.scss";
 
+import BasketIcon from "@/assets/icons/basket-icon.svg";
+import AvatarIcon from "@/assets/icons/avatar-icon.svg";
+
 import Link from "next/link";
 
 interface IHeaderProps {}
@@ -11,7 +14,24 @@ export function Header({}: IHeaderProps) {
                 <p>NAS</p>
                 <p className="header__logo-text__rotate">SHOP</p>
             </Link>
-            <div className="header__buttons"></div>
+            <div className="header__links">
+                {
+                    <>
+                        <Link href="/orders" className="header__links__order">
+                            <BasketIcon width={65} height={65} />
+                            Order
+                        </Link>
+                        <Link
+                            href="/registration"
+                            className="header__links__sign-up"
+                        >
+                            <AvatarIcon width={65} height={65} />
+                            Sign Up
+                        </Link>
+                    </>
+                }
+                {<Link href="/registration"></Link>}
+            </div>
         </header>
     );
 }
