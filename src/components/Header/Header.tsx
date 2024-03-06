0447f53ead0e72834c29ed89dff61be239bc435d
Taps73r <povroznykmisha@gmail.com>
2024-03-06 19:44:24 +0200

    end header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,9 +5,11 @@ import AvatarIcon from "@/assets/icons/avatar-icon.svg";
 
 import Link from "next/link";
 
-interface IHeaderProps {}
+interface IHeaderProps {
+    loginPage: boolean;
+}
 
-export function Header({}: IHeaderProps) {
+export function Header({ loginPage }: IHeaderProps) {
     return (
         <header className="header">
             <Link href="/main" className="header__logo-text">
@@ -15,22 +17,21 @@ export function Header({}: IHeaderProps) {
                 <p className="header__logo-text__rotate">SHOP</p>
             </Link>
             <div className="header__links">
-                {
-                    <>
-                        <Link href="/orders" className="header__links__order">
-                            <BasketIcon width={65} height={65} />
-                            Order
-                        </Link>
-                        <Link
-                            href="/registration"
-                            className="header__links__sign-up"
-                        >
-                            <AvatarIcon width={65} height={65} />
-                            Sign Up
-                        </Link>
-                    </>
-                }
-                {<Link href="/registration"></Link>}
+                <Link href="/orders" className="header__links__order">
+                    <BasketIcon width={65} height={65} />
+                    Order
+                </Link>
+                {loginPage ? (
+                    <Link
+                        href="/registration"
+                        className="header__links__sign-up"
+                    >
+                        <AvatarIcon width={65} height={65} />
+                        Sign Up
+                    </Link>
+                ) : (
+                    <Link href="/registration"></Link>
+                )}
             </div>
         </header>
     );
