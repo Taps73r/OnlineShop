833f1f27961e581c3836e1e07865ceead60e2021
Taps73r <povroznykmisha@gmail.com>
2024-03-09 23:16:02 +0200

    add ProductQuery and useEffect for set data and update

--- src/components/Product/ProductPage/ProductPage.tsx
+++ src/components/Product/ProductPage/ProductPage.tsx
@@ -1,17 +1,38 @@
-"use client"
+"use client";
 
 import { Header } from "@/components/Header/Header";
 import "./ProductPage.scss";
 
 import { useProductsQuery } from "@/hooks/useProductsQuery";
+import { useEffect, useState } from "react";
+import { IProduct } from "@/interface/products.interface";
+import { ProductContainer } from "../ProductContainer/ProductContainer";
 
 export function ProductPage(): JSX.Element {
-    const productQuery = useProductsQuery();
+    const [page, setPage] = useState<number>(0);
+
+    const ProductQuery = useProductsQuery(page);
+
+    const [productData, setProductData] = useState<IProduct[]>([]);
+
+    useEffect(() => {
+        void ProductQuery.refetch();
+    }, [page]);
+
+    useEffect(() => {
+        if (ProductQuery.data) {
+            setProductData((prevData) => [
+                ...prevData,
+                ...ProductQuery.data.content,
+            ]);
+        }
+    }, [ProductQuery.data?.content]);
 
     return (
         <div className="product-page">
             <Header loginPage={false} />
-            
+            <ProductContainer productData={productData} />
+            <button onClick={() => setPage(1)}>More</button>
         </div>
     );
 }
